1bb3f5bef3ef4fd6286507b3b8c8b820c1991d61
mahmoumaima <mahmoumaima@gmail.com>
2025-02-06 15:08:45 +0100

    texte apropos

--- frontend/src/pages/apropos/Apropos.jsx
+++ frontend/src/pages/apropos/Apropos.jsx
@@ -11,16 +11,16 @@ function Apropos() {
       </section>
       <section className={styles.collapsList}>
         <Collaps title="Fiabilité" key="collaps-1">
-          <p>This is the content of the collapsible section. You can put any HTML here.</p>
+          <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
         </Collaps>
         <Collaps title="Respect" key="collaps-2">
-          <p>This is the content of the collapsible section. You can put any HTML here.</p>
+          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
         </Collaps>
         <Collaps title="Service" key="collaps-3">
-          <p>This is the content of the collapsible section. You can put any HTML here.</p>
+          <p>La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
         </Collaps>
         <Collaps title="Sécurité" key="collaps-4">
-          <p>This is the content of the collapsible section. You can put any HTML here.</p>
+          <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l’hôte qu’au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
         </Collaps>
       </section>
    
